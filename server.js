59c2b83d698502f547542d95ad7e5a9281f3d415
rsmith590 <rsmith5901@gmail.com>
2016-05-02 22:09:51 +0000

    working

--- server.js
+++ server.js
@@ -68,12 +68,7 @@ passport.deserializeUser(function(id, done) {
 });
   
   
-app.use(function(req, res, next) {
-  if(!req.secure) {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  next();
-});
+
   
 var server = http.createServer(app);
 
