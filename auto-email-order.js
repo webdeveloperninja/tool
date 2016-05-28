57f28344958b0cbf71b444bb30ed1e50fabf35b1
rsmith590 <rsmith5901@gmail.com>
2016-05-28 02:55:17 +0000

    email functionality working now

--- auto-email-order.js
+++ auto-email-order.js
@@ -6,7 +6,7 @@ var exports = module.exports = {
     emailOrder: function(tool, userObj) {
         var options = {
           auth: {
-            api_user: 'webdeveloperninja1',
+            api_user: 'rsmith5901',
             api_key: '321eaglecourt'
           }
         }
@@ -19,8 +19,7 @@ var exports = module.exports = {
           to: userObj.toolingRep.email,
           subject:  userObj.companyName + ' Needs Tooling',
           html: '<b>' + userObj.companyName + ' needs ' + amountNeeded  + '</b>' + 
-                    '<p>' + tool.diameter + '" ' + tool.brand + ' ' + tool.toolType +  '</p><br>' + 
-                    '<b>For Customer Id: ' + userObj._id + '</b>'
+                    '<p>' + tool.toolName + '</p><br>'
         };
         
         client.sendMail(email, function(err, info){
