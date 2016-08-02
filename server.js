fbd840507eb263a7f14882c4f6ec3c76a02daeaf
rsmith590 <rsmith5901@gmail.com>
2016-08-02 00:36:37 +0000

    test

--- server.js
+++ server.js
@@ -60,15 +60,6 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-function requireHTTPS(req, res, next) {
-    if (!req.secure) {
-        //FYI this should work for local development as well
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
-app.use(requireHTTPS);
   
 var server = http.createServer(app);
 
