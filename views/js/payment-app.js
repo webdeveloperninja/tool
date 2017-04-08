e0a718563b164fcd5552e9805e551072f5cdb9ef
Your Name <rsmith5901@gmail.com>
2017-04-07 21:41:28 -0400

    1) Added credit card formating 2) Added user fields client/server

--- views/js/payment-app.js
+++ views/js/payment-app.js
@@ -11,6 +11,15 @@ paymentApp.init = function () {
 	$('#noticePayment').hide();
 	$('#notice').css({display: 'none'});
 
+	$('[data-numeric]').payment('restrictNumeric');
+	$('.cc-number').payment('formatCardNumber');
+	$('.cc-exp').payment('formatCardExpiry');
+	$('.cc-cvc').payment('formatCardCVC');
+	$.fn.toggleInputError = function(erred) {
+		this.parent('.form-group').toggleClass('has-error', erred);
+		return this;
+	};
+
 	if(paymentApp.testMode) {
 		paymentApp.setStripeTestMode();
 	}
@@ -25,6 +34,14 @@ paymentApp.init = function () {
 	$form.submit(function (event) {
 		// Improve validation
 		if (paymentApp.validPassword && !paymentApp.usernameExists) {
+			var cardType = $.payment.cardType($('.cc-number').val());
+			$('.cc-number').toggleInputError(!$.payment.validateCardNumber($('.cc-number').val()));
+			$('.cc-exp').toggleInputError(!$.payment.validateCardExpiry($('.cc-exp').payment('cardExpiryVal')));
+			$('.cc-cvc').toggleInputError(!$.payment.validateCardCVC($('.cc-cvc').val(), cardType));
+			$('.cc-brand').text(cardType);
+			$('.validation').removeClass('text-danger text-success');
+			$('.validation').addClass($('.has-error').length ? 'text-danger' : 'text-success');
+
 			$form.find('.submit').prop('disabled', true);
 
 			// Request a token from Stripe: this is the submit method
